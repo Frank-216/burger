de1fbeb39d75d2eb7663f86b3df35d9d6ac06d48
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-08-04 18:38:44 -0400

    added views items and finished the controller to the best of my ability

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,7 @@
 
 /*
 Here is where you create all the functions that will do the routing for your app, and the logic of each route.
+// still need to do more work on this controller page. 
 */
 var express 	= require('express');
 var bodyParser 	= require('body-parser');
@@ -10,9 +11,35 @@ var burger= require('../models/burger.js');
 
 // create a new router 
 var router = express.Router();
-
+// get funciton to display the home page 
 router.get('/',function(req,res){
 	res.redirect('/burgers')
 })
+// Get function to render everything held in the burger database
+router.get("/cats",function(req,res){
+	// call selectAll function with the paramater CB function
+	burger.selectAll(function(data){
+		var cbObject = {burgers:data};
+		console.log(cbObject);
+		res.render("index",bsObject);
+	})
+})
+// Post function to add information to the datebase 
+router.post("/burgers/create", function(req,res){
+	// takes the valude of the text box and adds it to the array 
+	burger.insertOne(val1,function(data){
+		res.redirect("/cats")
+	});
+});
+// updates the valude of devaured 
+router.put("/burgers/update/:id", function(req,res){
+	// take the id of value that is to be updated 
+	var id = req.params.id;
+	// pass along the ID of the value you want to update to the updateOne function 
+	burger.updateOne(id, function(){
+		res.redirect("/cats");
+	})
+});
 
-router.get()
+// Export Router
+module.exports = router;
