a6c38a960c0f1be87ab8975a71259782de09bde3
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-08-09 18:42:26 -0400

    Still working on the jaws

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(express.static('/views/assets/'));
 
 // override with POST having ?_method=DELETE
 app.use(method('_method'));
